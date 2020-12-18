97615c123ffc564ba5f91ba2cf8d04867dab7aa5
chenk <chenk@apical.com.cn>
2020-12-18 16:59:23 +0800

    update md5.c

--- md5.c
+++ md5.c
@@ -1,41 +1,26 @@
-// °üº¬Í·ÎÄ¼ş
+ï»¿// åŒ…å«å¤´æ–‡ä»¶
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include "md5.h"
 
-// ÀàĞÍ¶¨Òå
-typedef unsigned char      uint8_t;
-typedef unsigned int       uint32_t;
-typedef unsigned long long uint64_t;
-
-// ³£Á¿¶¨Òå
+// å¸¸é‡å®šä¹‰
+#define MD5_BLOCK_SIZE  64
 #define F(x, y, z)  ((x & y) | (~x & z))
 #define G(x, y, z)  ((x & z) | ( y &~z))
-#define H(x, y, z)  (x^y^z)
-#define I(x, y, z)  (y^(x|~z))
+#define H(x, y, z)  (x ^ y ^ z)
+#define I(x, y, z)  (y ^ (x | ~z))
 
-// ÄÚ²¿º¯Êı
+// å†…éƒ¨å‡½æ•°
 static uint32_t rotate_left(uint32_t x, int s) { return (x << s) | (x >> (32 - s)); }
-static uint32_t FF(uint32_t *w, uint32_t x, int s, uint32_t t) { return (w[1] + rotate_left(w[0] + F(w[1], w[2], w[3]) + x + t, s)); }
-static uint32_t GG(uint32_t *w, uint32_t x, int s, uint32_t t) { return (w[1] + rotate_left(w[0] + G(w[1], w[2], w[3]) + x + t, s)); }
-static uint32_t HH(uint32_t *w, uint32_t x, int s, uint32_t t) { return (w[1] + rotate_left(w[0] + H(w[1], w[2], w[3]) + x + t, s)); }
-static uint32_t II(uint32_t *w, uint32_t x, int s, uint32_t t) { return (w[1] + rotate_left(w[0] + I(w[1], w[2], w[3]) + x + t, s)); }
+static uint32_t FF(uint32_t w0, uint32_t w1, uint32_t w2, uint32_t w3, uint32_t x, int s, uint32_t t) { return (w1 + rotate_left(w0 + F(w1, w2, w3) + x + t, s)); }
+static uint32_t GG(uint32_t w0, uint32_t w1, uint32_t w2, uint32_t w3, uint32_t x, int s, uint32_t t) { return (w1 + rotate_left(w0 + G(w1, w2, w3) + x + t, s)); }
+static uint32_t HH(uint32_t w0, uint32_t w1, uint32_t w2, uint32_t w3, uint32_t x, int s, uint32_t t) { return (w1 + rotate_left(w0 + H(w1, w2, w3) + x + t, s)); }
+static uint32_t II(uint32_t w0, uint32_t w1, uint32_t w2, uint32_t w3, uint32_t x, int s, uint32_t t) { return (w1 + rotate_left(w0 + I(w1, w2, w3) + x + t, s)); }
 
-// º¯ÊıÊµÏÖ
-void md5_digest(uint8_t md5[16], uint8_t *data, int len)
+static void md5_process_block(uint32_t w[4], uint32_t x[16])
 {
-    int hlen = len & ~0x3f;
-    int llen = len &  0x3f;
-
-    uint32_t W[4], WW[9], X[16], *MW, *t;
-    uint32_t padding_buf[32];
-    int      padding_flag = llen < 56 ? 1 : 2;
-    int      process_flag = 0;
-    int      i, k;
-    uint8_t *psrc, *pend;
-
-    static uint32_t T[64] =
-    {
+    static const uint32_t T[64] = {
         0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee, 0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
         0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be, 0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
         0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa, 0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
@@ -45,60 +30,92 @@ void md5_digest(uint8_t md5[16], uint8_t *data, int len)
         0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039, 0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
         0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1, 0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391,
     };
-    static int SFF[4] = { 7, 12, 17, 22 };
-    static int SGG[4] = { 5, 9 , 14, 20 };
-    static int SHH[4] = { 4, 11, 16, 23 };
-    static int SII[4] = { 6, 10, 15, 21 };
-
-    // init W & MW
-    W[0] = 0x67452301; W[1] = 0xefcdab89; W[2] = 0x98badcfe; W[3] = 0x10325476;
-    MW   = (uint32_t*)WW + 4;
-
-    // append padding bits & length to padding buffer
-    memcpy((uint8_t*)padding_buf + 0   , data + hlen,       llen);
-    memset((uint8_t*)padding_buf + llen, 0          , 128 - llen);
-    ((uint8_t *)padding_buf)[llen] = 0x80;
-    ((uint64_t*)padding_buf)[8 * padding_flag - 1] = len * 8;
-
-process_handler:
-    // init psrc, pend
-    if (!process_flag) { psrc = data; pend = psrc + hlen; }
-    else { psrc = (uint8_t*)padding_buf; pend = psrc + 64 * padding_flag; }
-
-    while (psrc < pend)
-    {
-        // copy 512 bits data into X.
-        memcpy(X, psrc, 64); psrc += 64;
-
-        // save W
-        for (i=0; i<4; i++) WW[i+0] = WW[i+4] = W[i];
-
-        t = T; // init t
-        for (i=0,k=0; i<16; i++,k+=1,k&=0xf) MW[-(i&3)] = MW[4-(i&3)] = FF(&(MW[-(i&3)]), X[k], SFF[i&3], *t++); // round 1
-        for (i=0,k=1; i<16; i++,k+=5,k&=0xf) MW[-(i&3)] = MW[4-(i&3)] = GG(&(MW[-(i&3)]), X[k], SGG[i&3], *t++); // round 2
-        for (i=0,k=5; i<16; i++,k+=3,k&=0xf) MW[-(i&3)] = MW[4-(i&3)] = HH(&(MW[-(i&3)]), X[k], SHH[i&3], *t++); // round 3
-        for (i=0,k=0; i<16; i++,k+=7,k&=0xf) MW[-(i&3)] = MW[4-(i&3)] = II(&(MW[-(i&3)]), X[k], SII[i&3], *t++); // round 4
-
-        // increase W
-        for (i=0; i<4; i++) W[i] += MW[i];
-    }
-
-    // goto process_handler if necessary
-    if (!process_flag) { process_flag = 1; goto process_handler; }
-
-    // copy result
-    memcpy(md5, W, 16);
+    static const int SFF[4] = { 7, 12, 17, 22 };
+    static const int SGG[4] = { 5, 9 , 14, 20 };
+    static const int SHH[4] = { 4, 11, 16, 23 };
+    static const int SII[4] = { 6, 10, 15, 21 };
+    uint32_t wt[4] = { w[0], w[1], w[2], w[3] }, *t, i, k;
+    t = (uint32_t*)T;
+    for (i=0,k=0; i<16; i++,k+=1) wt[(4-i)&3] = FF(wt[(4-i)&3], wt[(1-i)&3], wt[(2-i)&3], wt[(3-i)&3], x[k&0xf], SFF[i&3], *t++); // round 1
+    for (i=0,k=1; i<16; i++,k+=5) wt[(4-i)&3] = GG(wt[(4-i)&3], wt[(1-i)&3], wt[(2-i)&3], wt[(3-i)&3], x[k&0xf], SGG[i&3], *t++); // round 2
+    for (i=0,k=5; i<16; i++,k+=3) wt[(4-i)&3] = HH(wt[(4-i)&3], wt[(1-i)&3], wt[(2-i)&3], wt[(3-i)&3], x[k&0xf], SHH[i&3], *t++); // round 3
+    for (i=0,k=0; i<16; i++,k+=7) wt[(4-i)&3] = II(wt[(4-i)&3], wt[(1-i)&3], wt[(2-i)&3], wt[(3-i)&3], x[k&0xf], SII[i&3], *t++); // round 4
+    for (i=0; i<4; i++) w[i] += wt[i];
 }
 
+typedef struct {
+    uint32_t w[4];
+    uint8_t  buffer[MD5_BLOCK_SIZE];
+    uint32_t size_total;
+    uint32_t size_buffer;
+} CONTEXT;
+
+void* md5_init(void)
+{
+    CONTEXT *context = calloc(1, sizeof(CONTEXT));
+    if (!context) return NULL;
+    md5_reset(context);
+    return context;
+}
+
+void md5_free (void *ctxt) { if (ctxt) free(ctxt); }
+
+void md5_reset(void *ctxt)
+{
+    CONTEXT *context = (CONTEXT*)ctxt;
+    if (!ctxt) return;
+    context->w[0] = 0x67452301;
+    context->w[1] = 0xefcdab89;
+    context->w[2] = 0x98badcfe;
+    context->w[3] = 0x10325476;
+    context->size_total = context->size_buffer = 0;
+}
+
+void md5_data(void *ctxt, uint8_t *data, int size)
+{
+    CONTEXT *context = (CONTEXT*)ctxt;
+    if (!ctxt) return;
+    while (size > 0) {
+        int n = size < MD5_BLOCK_SIZE - context->size_buffer ? size : MD5_BLOCK_SIZE - context->size_buffer;
+        memcpy(context->buffer + context->size_buffer, data, n);
+        data += n; size -= n; context->size_buffer += n; context->size_total += n;
+        if (context->size_buffer == MD5_BLOCK_SIZE) {
+            md5_process_block(context->w, (uint32_t*)context->buffer);
+            context->size_buffer = 0;
+        }
+    }
+}
+
+void md5_done(void *ctxt, uint8_t md5[16])
+{
+    CONTEXT *context = (CONTEXT*)ctxt;
+    if (!ctxt) return;
+    memset(context->buffer + context->size_buffer, 0, MD5_BLOCK_SIZE - context->size_buffer);
+    context->buffer[context->size_buffer] = 0x80;
+    if (context->size_buffer > 56) {
+        md5_process_block(context->w, (uint32_t*)context->buffer);
+        memset(context->buffer, 0, MD5_BLOCK_SIZE);
+    }
+    ((uint64_t*)context->buffer)[7] = context->size_total * 8;
+    md5_process_block(context->w, (uint32_t*)context->buffer);
+    memcpy(md5, context->w, sizeof(context->w));
+    md5_reset(ctxt);
+}
+
+#ifdef _TEST1_
 int main(void)
 {
-    uint8_t md5[16];
-    char   *str = "hello";
+    void   *md5ctx = md5_init();
+    uint8_t md5sum[16];
     int     i;
-
-    md5_digest(md5, (uint8_t*)str, (int)strlen(str));
-    for (i=0; i<16; i++) printf("%02X", md5[i]); printf("\n");
+    md5_data(md5ctx, (uint8_t*)"hello", 5);
+    md5_data(md5ctx, (uint8_t*)" "    , 1);
+    md5_data(md5ctx, (uint8_t*)"world", 5);
+    md5_done(md5ctx, md5sum);
+    for (i=0; i<16; i++) printf("%02X", md5sum[i]);
+    printf("\n");
+    md5_free(md5ctx);
     return 0;
 }
-
+#endif
 
---
+++ md5.h
@@ -0,0 +1,12 @@
+﻿#ifndef __MD5_H__
+#define __MD5_H__
+
+#include <stdint.h>
+
+void* md5_init (void);
+void  md5_free (void *ctxt);
+void  md5_reset(void *ctxt);
+void  md5_data (void *ctxt, uint8_t *data, int size);
+void  md5_done (void *ctxt, uint8_t  md5[16]);
+
+#endif
